0e142fd6fe6b2652fd7010e586e17a6f75c2ef91
manzsadesign <manzsadesign@gmail.com>
2026-06-20 23:53:18 +0700

    Send Festify emails immediately

--- app/Notifications/FestifyResetPassword.php
+++ app/Notifications/FestifyResetPassword.php
@@ -3,14 +3,10 @@
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\ResetPassword;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class FestifyResetPassword extends ResetPassword implements ShouldQueue
+class FestifyResetPassword extends ResetPassword
 {
-    use Queueable;
-
     public function toMail($notifiable): MailMessage
     {
         $url = url(route('password.reset', [
--- app/Notifications/FestifyVerifyEmail.php
+++ app/Notifications/FestifyVerifyEmail.php
@@ -3,17 +3,13 @@
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\URL;
 
-class FestifyVerifyEmail extends VerifyEmail implements ShouldQueue
+class FestifyVerifyEmail extends VerifyEmail
 {
-    use Queueable;
-
     public function toMail($notifiable): MailMessage
     {
         $url = $this->verificationUrl($notifiable);
